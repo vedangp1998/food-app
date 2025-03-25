305b11bbd4691da7ff07eb163e97910a416f451b
vedangp1998 <vedang.prajapati18@gmail.com>
2025-03-25 18:25:56 +0530

    add-notFound-page

---
+++ app/Error.tsx
@@ -0,0 +1,10 @@
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>Some Error Occured</h1>
+      <p>Failed to fetch meals Data!</p>
+    </main>
+  );
+};
+
+export default Error;
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <section>
+      <h1>Not Found</h1>
+      <p>Unfortunately, we could not find the Page!</p>
+    </section>
+  );
+};
+
+export default NotFound;
